8a5069013aa50ef083d142e9e00d90dc9d74b299
ranggakorompis <korompisrangga14@gmail.com>
2023-03-13 07:40:17 +0800

    create feature write NFC

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-// coba nfc
-const nfc = document.getElementById("nfc");
-if ("NDEFReader" in window) {
-  nfc.innerHTML = "Hello NFC!";
-} else {
-  nfc.innerHTML = "NFC tidak support!";
+// Cek NFC Support & Write something in NFC Tag
+async function writeNfc() {
+  if ("NDEFReader" in window) {
+    let nfc = (document.getElementById("nfc").innerHTML = "NFC supported!");
+    const ndef = new NDEFReader();
+    let arr1 = ["Rangga", 21];
+    try {
+      await ndef.write(arr1);
+      logData("Write NFC berhasil!");
+    } catch (error) {
+      logData("Write NFC gagal!");
+    }
+  } else {
+    nfc.innerHTML = "NFC tidak support!";
+  }
+}
+
+// Function untuk menampilkan log
+function logData(data) {
+  let logElement = (document.getElementById("log").innerHTML = data);
 }
