32de8bc93d3a6b6e387cb7089df8dc3e049833c8
ranggakorompis <korompisrangga14@gmail.com>
2023-03-13 11:36:51 +0800

    test write nfc again

--- index.js
+++ index.js
@@ -1,48 +1,19 @@
-// Cek NFC support
-// let nfc = document.getElementById("nfc");
-// if ("NDEFReader" in window) {
-//   nfc.innerHTML = "NFC supported!";
-// } else {
-//   nfc.innerHTML = "NFC not supported!";
-// }
-
-const dataUser = {
-  name: "Rangga",
-  age: 21,
-};
-
-function writeNfcTag() {
-  if ("NDEFMessage" in window) {
-    const message = new NDEFMessage([new NDEFRecord("text/plain", dataUser)]);
-    if ("NDEFWriter" in window) {
-      const writer = new NDEFWriter();
-      writer
-        .write(message)
-        .then(() => {
-          logData("Pesan berhasil di tulis di tag NFC");
-        })
-        .catch((error) => {
-          console.error("Pesan gagal di tulis!");
-        });
-    }
+// Write pesan sederhana pada nfc tag
+function writeNfc() {
+  if ("NDEFReader" in window) {
+    const ndef = new NDEFReader();
+    ndef
+      .write("Hello NFC")
+      .then(() => {
+        logData("Pesan berhasil di tulis!");
+      })
+      .catch((error) => {
+        logData(`Pesan gagal di tulis: coba lagi ${error}`);
+      });
   } else {
     logData("NFC tidak support!");
   }
 }
-
-// Write something in NFC Tag
-// async function writeNfc() {
-//   if ("NDEFReader" in window) {
-//     const ndef = new NDEFReader();
-//     let arr1 = ["Rangga", 21];
-//     try {
-//       await ndef.write(arr1);
-//       logData("Write NFC berhasil!");
-//     } catch (error) {
-//       logData("Write NFC gagal!");
-//     }
-//   }
-// }
 
 // Function untuk menampilkan log
 function logData(data) {
